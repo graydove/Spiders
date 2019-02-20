e9c051e7966b4c47daad1823bea8bd2149c9f32c
graydove <670332678@qq.com>
2019-02-20 09:59:05 +0800

    fix commit

--- test3.py
+++
@@ -1,7 +0,0 @@
-f = open('C:/Users/admin\Desktop\KeNan001_998.txt', 'r')
-g = open('C:/Users/admin\Desktop\Detective Conan.txt', 'a+')
-for i in f.readlines():
-    if 'playUrl' in i:
-        url = i.split(' ')[-1][1:-3]
-        g.write(url + '\n')
-        print(url)
